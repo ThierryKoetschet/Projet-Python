262799a94432676a614c87bfe7d0090f2d09c760
Thierry Koetschet <thierry.koetschet@cpnv.ch>
2022-03-29 10:01:50 +0200

    Finished version Fibonacci python

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+# Auteur :         Thierry Koetschet
+# Projet :         Suite de Fibonacci en Python
+# Version :        1.0
+# Date :           28.03.2022
+# Description :    Programme permettant d'afficher la suite de Fibonacci en demandant la longueur de la suite
+#                  à l'utilisateur et en utilisant les tableaux
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# variables utiles à l'affichage de la suite de fibonacci
+nombrePrecedent, nombreActuel = 0, 1
 
+# demande à l'utilisateur de donner une valeur pour la suite de fibonacci
+longueurFibonacci = int(input("Donnez une longueur pour la suite de fibonacci : "))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# vérifie que le nombre donné est un entier
+if longueurFibonacci <= 0:
+   print("Veuillez entrer un nombre positif")
+# si la longueur de la suite est de 1, retourne le nombreActuel
+elif longueurFibonacci == 1:
+   print("Fibonacci sequence upto",longueurFibonacci,":")
+   print(nombrePrecedent)
+# génère la suite de fibonacci
+else:
+   print(nombrePrecedent, end=' ')
+   for x in range(1, longueurFibonacci):
+       print(nombreActuel, end=' ')
+       nombreActuel += nombrePrecedent
+       nombrePrecedent = nombreActuel - nombrePrecedent
